c3b35d883cc0f492c882578b96abd1301df7d22f
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-24 16:04:49 -0400

    connecting outlets

---
+++ ForgetMeNot/Controllers/AddEventViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddEventViewController.swift
+//  ForgetMeNot
+//
+//  Created by DetroitLabs on 9/24/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class AddEventViewController: UITableViewController {
+
+    // MARK: - Properties
+    /// Labels:
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var recipientLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var haveGiftLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    /// UI Input Elements:
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var recipientField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var haveGiftSwitch: UISwitch!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Methods
+    
+    
+    // MARK: - Actions
+}
