16e1d5a1d6b3bf321439a8e7a9d431930598493c
Britney Smith <brit.smith7@gmail.com>
2018-09-25 10:08:34 -0400

    Added GiftIdeaLinks, cleaned up array formatting

--- ForgetMeNot/Model/EventDatabase.swift
+++ ForgetMeNot/Model/EventDatabase.swift
@@ -11,6 +11,5 @@ import Foundation
 class EventDatabase {
     var events = [Event(eventTitle: "Christmas", giftRecipient: "Bella", dateOfEventString: "12/25/2018", haveGift: false, eventNotes: "notes"),
                   Event(eventTitle: "Kyubei's 7th Birthday!", giftRecipient: "Kyubei", dateOfEventString: "04/25/2019", haveGift: false, eventNotes: "My baby..."),
-                  Event(eventTitle: "Mom's birthday", giftRecipient: "Mom", dateOfEventString: "05/07/2019", haveGift: false, eventNotes: "Don't forget!")
-    ]
+                  Event(eventTitle: "Mom's birthday", giftRecipient: "Mom", dateOfEventString: "05/07/2019", haveGift: false, eventNotes: "Don't forget!")]
 }
---
+++ ForgetMeNot/Model/GiftIdeaLinks.swift
@@ -0,0 +1,15 @@
+//
+//  GiftIdeaLinks.swift
+//  ForgetMeNot
+//
+//  Created by Britney Smith on 9/25/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import Foundation
+
+struct GiftIdeaLinks {
+    var urls = ["https://www.uncommongoods.com/gifts",
+                "https://www.thegrommet.com/gifts",
+                "https://www.gifts.com/"]
+}
