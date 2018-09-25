7b16da818f4b7331e074e727c67546cbb0fb26a1
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-25 09:46:48 -0400

    basic file setup

---
+++ ForgetMeNot/Controllers/EventDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EventDetailViewController.swift
+//  ForgetMeNot
+//
+//  Created by DetroitLabs on 9/25/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UITableViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+}
