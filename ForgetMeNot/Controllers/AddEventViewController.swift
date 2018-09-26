ac71e00c259b182273547eea358cde3408577f7c
Elle Gover <elle.gover@detroitlabs.com>
2018-09-26 16:43:58 -0400

    changing nav bar color

--- ForgetMeNot/Controllers/AddEventViewController.swift
+++ ForgetMeNot/Controllers/AddEventViewController.swift
@@ -53,6 +53,7 @@ class AddEventViewController: UITableViewController {
         super.viewDidLoad()
         renderSaveBarButton()
         self.view.backgroundColor = Settings.sharedService.lighterColor;
+        UINavigationBar.appearance().barTintColor = UIColor(red: 181/255.0, green: 198/255.0, blue: 255/255.0, alpha: 1.0)
         renderCancelBarButton()
         restrictDates()
     }
